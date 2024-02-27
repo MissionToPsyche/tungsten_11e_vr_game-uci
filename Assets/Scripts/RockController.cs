40e68ccf83ccca6d34256dde2753c365269b7ccf
TheChilledLemon <40331455+TheChilledLemon@users.noreply.github.com>
2024-02-27 01:31:15 -0800

    Cleaned up destruction code, fixed grab on pieces, and created prefabs for rock and pickaxe

--- Assets/Scripts/RockController.cs
+++ Assets/Scripts/RockController.cs
@@ -5,43 +5,19 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class RockController : MonoBehaviour
 {
-    public GameObject root;
-
-    void Awake()
-    {
-        XRGrabInteractable gi = gameObject.GetComponent<XRGrabInteractable>();
-        for (int i = 0; i < transform.childCount; i++) {
-            Transform child = transform.GetChild(i);
-            if (ReferenceEquals(child, root.transform)) {
-                continue;
-            }
-
-            //Rigidbody rb = child.gameObject.AddComponent<Rigidbody>();
-            //rb.isKinematic = true;
-
-            MeshCollider mc = child.gameObject.AddComponent<MeshCollider>();     
-            mc.convex = true;
-
-            gi.colliders.Add(mc);
-
-            //child.gameObject.AddComponent<RockPieceController>();
-        }
-        gi.interactionManager.UnregisterInteractable(gi.GetComponent<IXRInteractable>());
-        gi.interactionManager.RegisterInteractable(gi.GetComponent<IXRInteractable>());
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         Collider thisCollider = collision.GetContact(0).thisCollider;
         Collider otherCollider = collision.GetContact(0).otherCollider;
         if (thisCollider.transform.parent != null && otherCollider.gameObject.CompareTag("PickHead"))
         {
-            Debug.Log("Hit and breaking!");
+            XRGrabInteractable gip = gameObject.GetComponent<XRGrabInteractable>();
+            gip.interactionManager.UnregisterInteractable(gip.GetComponent<IXRInteractable>());
+            gip.colliders.Remove(thisCollider);
+            gip.interactionManager.RegisterInteractable(gip.GetComponent<IXRInteractable>());
             thisCollider.transform.parent = null;
-            Rigidbody rb = thisCollider.gameObject.AddComponent<Rigidbody>();
-            XRGrabInteractable gi = thisCollider.gameObject.AddComponent<XRGrabInteractable>();
-            MeshCollider mc = thisCollider.gameObject.GetComponent<MeshCollider>();
-            mc.isTrigger = false;
+            thisCollider.gameObject.AddComponent<Rigidbody>();
+            thisCollider.gameObject.AddComponent<XRGrabInteractable>();
         }
     }
 }
