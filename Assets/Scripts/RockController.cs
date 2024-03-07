e36b6399139e64cda34fa77dcd195246ccd51767
TheChilledLemon <40331455+TheChilledLemon@users.noreply.github.com>
2024-03-07 02:22:27 -0800

    Added rock textures, hit FX, and sound FX

--- Assets/Scripts/RockController.cs
+++ Assets/Scripts/RockController.cs
@@ -9,15 +9,26 @@ public class RockController : MonoBehaviour
     {
         Collider thisCollider = collision.GetContact(0).thisCollider;
         Collider otherCollider = collision.GetContact(0).otherCollider;
-        if (thisCollider.transform.parent != null && otherCollider.gameObject.CompareTag("PickHead"))
+        if (thisCollider.transform.parent != null && otherCollider.gameObject.CompareTag("PickHead") && collision.relativeVelocity.magnitude > 2)
         {
             XRGrabInteractable gip = gameObject.GetComponent<XRGrabInteractable>();
             gip.interactionManager.UnregisterInteractable(gip.GetComponent<IXRInteractable>());
             gip.colliders.Remove(thisCollider);
             gip.interactionManager.RegisterInteractable(gip.GetComponent<IXRInteractable>());
+
+            ParticleSystem ps = thisCollider.GetComponent<ParticleSystem>();
+            ps.Play();
+
+            AudioSource audioSource = thisCollider.GetComponent<AudioSource>();
+            audioSource.Play();
+
             thisCollider.transform.parent = null;
-            thisCollider.gameObject.AddComponent<Rigidbody>();
+            Rigidbody rb = thisCollider.gameObject.AddComponent<Rigidbody>();
+            rb.AddForce(collision.impulse, ForceMode.Impulse);
+            // Demo purposes
+            rb.useGravity = false;
             thisCollider.gameObject.AddComponent<XRGrabInteractable>();
+            
         }
     }
 }
