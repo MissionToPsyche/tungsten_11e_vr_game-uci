d880af168d7e2cbc776a1fb3017c93e0b52036e2
TheChilledLemon <40331455+TheChilledLemon@users.noreply.github.com>
2024-04-09 19:41:20 -0700

    Added GameManager and RockType skeleton

---
+++ Assets/Scripts/RockType.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Rock Type", menuName = "ScriptableObjects/RockTypeScriptableObject", order = 1)]
+public class RockType : ScriptableObject
+{
+    public string typeName;
+}
